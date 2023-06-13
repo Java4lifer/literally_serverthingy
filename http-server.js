c5d58abd04d8c0f17d75503c5f5642779885a261
Java4lifer <104568501+Java4lifer@users.noreply.github.com>
2023-06-13 02:33:56 +0000

    Terminado os quatro comandos e decorado o sistema. Adicionado uma função para verificar se o arquivo existe ou não.

--- http-server.js
+++ http-server.js
@@ -8,26 +8,36 @@ app.use("/write", (req, res) => {
     const { file, text } = req.query
     fs.writeFileSync(file, text)
     //write?file=chirno&text=baka
-    res.send(text) 
+    res.send(`O arquivo '${file}' foi criado com o seguinte conteudo: '${text}'`) 
   })
 
-app.use("/read", (req) => {
+app.use("/read", (req, res) => {
     const { file } = req.query
-    fs.readFileSync(req.query.file)
+    if(fs.existsSync(file)) {
+        res.send(`Lendo o arquivo: ${file} <br><br>'${fs.readFileSync(file)}'`)
+    }
+    else {res.send(`Arquivo '${file}' não existe ou não foi encontrado.`)}
 })
 
-app.use("/delete", (req) => {
+app.use("/delete", (req, res) => {
     const { file } = req.query
-    fs.rmFileSync(file, texto)
-    
+    fs.rmSync(file)
+    if(fs.existsSync(file)) {
+        res.send(`Arquivo '${file}' deletado com sucesso.`)
+    }
+    else {res.send(`Arquivo '${file}' não existe ou não foi encontrado.`)}
 })
 
-app.use("/patch", (req) => {
+app.use("/patch", (req, res) => {
     const {file, text} = req.query
-    fs.appendFileSync(file, text)
-
+    const textold = fs.readFileSync(file)
+    if(fs.existsSync(file)) {
+        fs.writeFileSync(file, text, {encoding:'utf8',flag:'w'})
+        res.send(`Conteudo do arquivo '${file}' foi alterado de: '${textold}', para: '${text}'`)
+    }
+    else {res.send(`Arquivo '${file}' não existe ou não foi encontrado.`)}
 })
-
-app.listen(3000, () => console.log("Servidor roda."))
+const port = 3000
+app.listen(port, () => console.log(`Servidor rodando de boa na porta: ${port}.`))
 
 //write, read, delete and update
