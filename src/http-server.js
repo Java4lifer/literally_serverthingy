62a1ec482978ee3ebdbb5efcf10a6f6ba8d89919
Java4lifer <104568501+Java4lifer@users.noreply.github.com>
2023-06-27 13:34:38 +0000

    quase terminado o patch

--- src/http-server.js
+++ src/http-server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const fs = require("fs")
 const { getDBinst } = require("./database")
+const { debugPort, title } = require("process")
 // destrutores
 // let x = ["hi", "hoi", "nein"]
 // let {, , german} = x
@@ -11,55 +12,84 @@ app.use(express.static(__dirname + '/../public'))
 app.use(express.json())
 //write?title=a&source=a&description=a&thumb=a
 
-app.use("/write", async (req, res) => {
-    const { id } = req.query
+app.post("/films", async (req, res) => {
     const { title, source, description, thumb } = req.body
+    
     const db = await getDBinst()
     const result = await db.run(
         `insert into films(title, source, description, thumb) values(?,?,?,?)`, 
         [title, source, description, thumb]
         )
-    res.send(result) 
+    res.json(result) 
   })
 
-app.use("/read", async (req, res) => {
+app.get("/films", async (req, res) => {
     const { id } = req.query
     const db = await getDBinst()
     if(id){
         const result = await db.get(
-            `select * from films where id is ?`,
-            [id]
+            `select * from films where id = ?`,
+            id
         )
-        res.send(result)
+        res.json(result)
+        return
     }
+    const result = await db.all(`select * from films`)
+    res.send(result)
 })
 
-app.use("/delete", async (req, res) => {
+app.delete("/films", async (req, res) => {
     const { id } = req.query
     const db = await getDBinst()
     if(id){
         const result = await db.run(
             `delete from films where id = ?`,
-            [id]
+            id
         )
         res.send(`Filme deletado.`)
         }
 })
 
-app.use("/patch", async (req, res) => {
-    const { id, title, source, description, thumb } = req.query
+app.put("/films", async (req, res) => {
+    const { id } = req.query
+    const { title, source, description, thumb } = req.body
     const db = await getDBinst()
     if(id){
         const result = await db.run(
             `update films
-            set title = ?;
-                source = ?;
-                description = ?;
+            set title = ?,
+                source = ?,
+                description = ?,
                 thumb = ?
             where id = ?`,
-            [id],
+            [title, source, description, thumb, id]
         )
-        res.send(result)
+        res.json(result)
+        }
+})
+
+app.patch("/films", async (req, res) => {
+    const { id } = req.query
+    const alts = []
+    const reqs = req.body
+    const db = await getDBinst()
+    for(objs in req.body) {
+        alts.push(`${objs} = ?`)
+        //reqs.push(objs)
+    }
+    const altstr = alts.toString()
+    console.log(`${altstr}\n${reqs}`)
+    if(id){
+        const result = await db.run(
+           `update films
+            set ${altstr}
+            where id = ?`,
+            [reqs, id]
+      )
+        res.json(reqs)
+        for(var keys in reqs){
+            console.log(keys) //source, thumb
+            console.log(reqs[keys]) //Touhou Project 12.8, None
         }
 })
 
